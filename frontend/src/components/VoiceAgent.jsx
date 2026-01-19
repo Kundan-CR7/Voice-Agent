02535aab4d0ac5d42a1c9b26aaf553e22bbc7148
Kundan-CR7 <Kundangupta7334@gmail.com>
2026-01-20 01:05:55 +0530

    feat: add microphone access

--- frontend/src/components/VoiceAgent.jsx
+++ frontend/src/components/VoiceAgent.jsx
@@ -4,6 +4,27 @@ const VoiceAgent = () => {
     const [userId,setUserId] = useState(null)
     const [status,setStatus] = useState("disconnected")
     const wsRef = useRef(null)
+    const [isRecording,setIsRecording] = useState(false)
+    const audioContextRef = useRef(null)
+    const streamRef = useRef(null)
+    const processorRef = useRef(null)
+
+
+    const startRecording = async() => {
+        try{
+            streamRef.current = await navigator.mediaDevices.getUserMedia({audio:true})
+            audioContextRef.current = new AudioContext({sampleRate:16000})
+            setIsRecording(true)
+        }catch(error){
+            console.log("Mic access denied: ",error)
+        }
+    }
+
+    const stopRecording = async() => {
+        streamRef.current?.getTracks().forEach(track => track.stop())
+        audioContextRef.current?.close()
+        setIsRecording(false)
+    }
 
     useEffect(() => {
         wsRef.current = new WebSocket("ws://localhost:3000")
@@ -20,12 +41,18 @@ const VoiceAgent = () => {
             setStatus("disconnected")
         }
         return () => wsRef.current?.close()
-    })
+    },[])
 
   return (
     <div className='bg-gray-800 p-8 rounded-lg shadow-xl'>
         <h1 className='text-2xl font-bold mb-4'>Voice Agent</h1>
         <div className='space-y-2'>
+            <button 
+                onClick={isRecording ? stopRecording : startRecording}
+                className={`mt-6 px-6 py-3 rounded-lg font-semibold ${isRecording ? 'bg-red-600 hover:bg-red-700' : 'bg-green-600 hover:bg-green-700'}`}
+            >
+                {isRecording ? 'Stop Recording' : 'Start Recording'}
+            </button>
             <p>Status: <span className={status=="connected"? "text-green-400" : "text-red-400" }>{status}</span></p>
             <p>User ID: <><span className='text-blue-400'>{userId || "Waiting"}</span></> </p>
         </div>
